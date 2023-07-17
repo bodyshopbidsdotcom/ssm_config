87271d1959d8af5c18b71b840a11494723dc6ab2
franchester4 <lukee.choi21@gmail.com>
2023-07-17 15:22:18 -0400

    remove lib

--- lib/ssm_config.rb
+++ lib/ssm_config.rb
@@ -1,13 +1,13 @@
-require './lib/ssm_config/ssm_storage/db.rb'
-require './lib/ssm_config/ssm_storage/yml.rb'
-require './lib/ssm_config/ssm_storage/empty.rb'
-require './lib/ssm_config/errors.rb'
-require './lib/ssm_config/migration_helper.rb'
+require 'ssm_config/ssm_storage/db.rb'
+require 'ssm_config/ssm_storage/yml.rb'
+require 'ssm_config/ssm_storage/empty.rb'
+require 'ssm_config/errors.rb'
+require 'ssm_config/migration_helper.rb'
 require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/time'
 
 module SsmConfig
-  VERSION = '1.2.0'.freeze
+  VERSION = '1.2.1'.freeze
   REFRESH_TIME = (30.minutes).freeze
 
   class << self
