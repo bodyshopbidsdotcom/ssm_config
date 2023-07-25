02e63ad0069ab622401ea8fb39fe753d99eb51b4
franchester4 <lukee.choi21@gmail.com>
2023-07-25 15:52:44 -0400

    fixed logic around erb

--- lib/ssm_config/migration_helper.rb
+++ lib/ssm_config/migration_helper.rb
@@ -49,10 +49,15 @@ module SsmConfig
       end
     end
 
+    def determine_erb(value)
+      return 'erb' if (value[0..2] == '<%=') && (value[-2..-1] == '%>')
+      return 'string'
+    end
+
     def determine_class(value)
       return 'boolean' if (value == false) || (value == true)
-      return 'erb' if (value[0..2] == '<%=') && (value[-2..-1] == '%>')
-      value.class
+      return value.class unless value.is_a? String
+      return determine_erb(value)
     end
 
     def file_path
